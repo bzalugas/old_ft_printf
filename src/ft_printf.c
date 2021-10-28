a580cc73cc57aae9b8d04be6d773a471a3da32a7
Bastien <bastien.zalugas@student.42.fr>
2021-10-29 01:20:19 +0200

    Start a simple test for variadic function

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,13 +6,27 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 19:29:17 by bzalugas          #+#    #+#             */
-/*   Updated: 2021/10/28 20:11:27 by bzalugas         ###   ########.fr       */
+/*   Updated: 2021/10/29 00:27:44 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int	ft_printf(const char *str, ...)
 {
+	va_list	list;
+	int		i;
+
+	va_start(list, str);
+	i = -1;
+	while (str[++i] && str[i] != '%')
+		ft_putchar_fd(str[i],1);
+	if (str[i] == '%')
+		i++;
+	if (str[i] && str[i] == 's')
+		ft_putstr_fd(va_arg(list, char *), 1);
+	while (str[++i])
+		ft_putchar_fd(str[i], 1);
 	return (0);
 }
--- src/ft_printf.h
+++ src/ft_printf.h
@@ -1,6 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 19:30:36 by bzalugas          #+#    #+#             */
+/*   Updated: 2021/10/29 00:24:11 by bzalugas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include "../libft/libft.h"
+# include <stdarg.h>
 
 int	ft_printf(const char *str, ...);
 
