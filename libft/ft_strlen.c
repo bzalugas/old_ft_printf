11fb1190c7ede1a19495627fd9280ff1b8519ff7
Bastien <bastien.zalugas@student.42.fr>
2021-10-30 04:00:38 +0200

    Correct some big error in strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 01:56:00 by bzalugas          #+#    #+#             */
-/*   Updated: 2021/01/25 21:29:35 by bzalugas         ###   ########.fr       */
+/*   Updated: 2021/10/30 03:58:48 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t i;
 
 	i = 0;
-	while (s[i])
+	while (s && s[i])
 		i++;
 	return (i);
 }
