66898a08c25aaf80368a516b9f9998d81d111509
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2022-01-23 22:25:33 +0100

    Correct stupid error

--- src/handle_hexa.c
+++ src/handle_hexa.c
@@ -6,7 +6,7 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 18:19:38 by bzalugas          #+#    #+#             */
-/*   Updated: 2022/01/22 18:44:11 by bzalugas         ###   ########.fr       */
+/*   Updated: 2022/01/22 19:51:10 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/ft_printf.h"
@@ -31,6 +31,6 @@ void	handle_hexa(unsigned int n, t_flags *flags, t_buffer *buf)
 	hexa = NULL;
 	hexa = pointer_to_hexa(n, upper_case);
 	if (hexa && flags->hashtag)
-		buffer_add_str(buf, pre, 0, 3);
+		buffer_add_str(buf, pre, 0, 2);
 	buffer_add_str(buf, hexa, 0, -1);
 }
