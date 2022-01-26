8fc95f16ef8646f9605db1dc6a06634f539bfac5
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2022-01-26 20:29:10 +0100

    Correct handle_decimal

--- src/handle_decimal.c
+++ src/handle_decimal.c
@@ -6,7 +6,7 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 15:03:30 by bzalugas          #+#    #+#             */
-/*   Updated: 2022/01/26 16:29:23 by bzalugas         ###   ########.fr       */
+/*   Updated: 2022/01/26 20:28:06 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 void	handle_decimal(int n, t_flags *flags, t_buffer *buf)
 {
 	int	i;
+	int	len;
 
 	i = -1;
+	len = ft_get_pow(n, 1);
 	if (flags->space && !flags->dot && !flags->plus)
 		buffer_add_char(buf, ' ');
 	else if (flags->plus && n > 0)
 		buffer_add_char(buf, '+');
 	else if (flags->dot)
-		while (++i < (flags->precision - ft_get_pow(n, 1)))
+		while (++i < (flags->precision - len))
 			buffer_add_char(buf, '0');
-	else if (flags->space)
-		buffer_add_char(buf, ' ');
+	/* else if (flags->space) */
+	/* 	buffer_add_char(buf, ' '); */
 	buffer_add_str(buf, ft_itoa(n), 0, -1);
 }
